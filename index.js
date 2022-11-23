8b68f10b2e191404fa003830a8a36a2b49592e25
brian <brian.ogango@student.moringaschool.com>
2022-11-23 08:39:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-const  currentUser = 'BRIAN'
+const  currentUser = 'Brian'
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
 const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!` ;
